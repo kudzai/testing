952d89ea4ce1c6e6621e1002b87a194c4a580e38
Paul Katsande <paul.katsande@gmail.com>
2015-11-15 12:54:59 +0000

    failing test

--- src/test_hitcount.py
+++ src/test_hitcount.py
@@ -14,6 +14,7 @@ class HitCountTest(unittest.TestCase):
     def testOneHit(self):
         # increase the hit count for user1
         hitcount.hit("user1")
+        hitcount.hit("user1")
         # ensure that the hit count for user1 is just 1
         self.assertEqual(b'1', hitcount.getHit("user1"))
 
